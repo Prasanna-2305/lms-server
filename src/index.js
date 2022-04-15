70b0ff8306d34c1b4d24ec2148a33da50136b536
Prasanna-2305 <pbhagwat@bestpeers.com>
2022-04-15 18:18:13 +0530

    node-admin-side-code

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ import courseRouter from "./routes/course.js";
 import cors from "cors";
 const index = express()
 const port = process.env.PORT || 8001;
+index.use(express.static('public'))
+index.get('/', (req, res) => {
+    res.send("Hello world")
+})
 index.use(express.json())
 index.use(cors())
 index.use("/users", router)
 index.use("/addcourse", courseRouter)
-//index.use("/admin", adminRoute)
+// index.use("/admin", adminRoute)
 index.listen(port, function () {
     console.log(`connected port ${port}`);
 })
--- src/routes/course.js
+++ src/routes/course.js
@@ -1,47 +1,82 @@
-import express  from "express";
+import express from "express";
 import AddCourse from "../models/adminCourse.js";
 import multer from "multer";
 const courseRouter = express.Router()
 
 const storage = multer.diskStorage({
-    destination: (req, file, callback) =>{
-        callback(null, './src/uploads/images')
+  destination: (req, file, callback) => {
+    callback(null, '/public/images')
 
-    },
-    filename: (req, file, callback) => {
-        callback(null, file.originalname)
-        
-    }
+  },
+  filename: (req, file, callback) => {
+    callback(null, file.originalname)
+
+  }
 })
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
 courseRouter.post("/create", upload.single("image"), async (req, res) => {
-    try {
-      const user = new AddCourse({
-        title: req.body.title,
-        content: req.body.content,
-        video: req.body.video,
-        file: req.file.originalname,
-      });
-      
-      console.log(req.body);
-      console.log(req.file)
-      const insert = await user.save();
-      res.status(201).send(insert);
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  });
-
   
-  courseRouter.get("/read", async (req, res) => {
-    try {
-      const user = req.params
-      const users = await AddCourse.find(user);
-      res.send(users)
-    } catch (e) {
-      res.status(400).send(e);
-    }
-  })
+  try {
+    // const imageUrl = `http://localhost:8001/src/uploads/images/${req.file.originalname}`;
+    
+    const link = req.protocol + '://' + req.get('host')
+    const imageUrl = link + req.file.originalname;
+    
+    const user = new AddCourse({
+      title: req.body.title,
+      content: req.body.content,
+      video: req.body.video,
+      file: imageUrl,
+    });
+    console.log(req.body);
+    console.log(req.file)
+    const insert = await user.save();
+    res.status(201).send(insert);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+
+courseRouter.get("/view", async (req, res) => {
+  try {
+    const user = req.params
+    const users = await AddCourse.find(user);
+    res.send(users)
+  } catch (e) {
+    res.status(400).send(e);
+  }
+})
+courseRouter.get("/view/:_id", async (req, res) => {
+  try {
+    const userid = req.params._id
+    const user = await AddCourse.findById(userid);
+    res.send(user)
+  } catch (e) {
+    res.status(400).send(e);
+  }
+})
+courseRouter.put('/update/:_id', async (req, res) => {
+  try {
+    //   const userid = 
+    const user = await AddCourse.findByIdAndUpdate(req.params._id, req.body, {
+      new: true
+    })
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+    console.log(res.status(400).send(e));
+  }
+})
+
+courseRouter.delete('/delete/:_id', async (req, res) => {
+  try {
+    const users = await AddCourse.findByIdAndDelete({ _id: req.params._id })
+    res.status(201).send({ message: "deleted!!" });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+})
 
 export default courseRouter;
