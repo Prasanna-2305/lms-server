0b5e2e5b6f79eaff6853619bf7d847cd31d77ce9
Prasanna-2305 <pbhagwat@bestpeers.com>
2022-04-19 15:59:36 +0530

    node-changes

--- src/routes/course.js
+++ src/routes/course.js
@@ -18,16 +18,16 @@ const upload = multer({ storage: storage });
 courseRouter.post("/create", upload.single("image"), async (req, res) => {
   
   try {
-   
+    
     
     const link = req.protocol + '://' + req.get('host')
-    const imageUrl = link + '/' + req.file.originalname;
+    const imagePath = link + '/' + req.file.originalname;
     
     const user = new AddCourse({
       title: req.body.title,
       content: req.body.content,
       video: req.body.video,
-      file: imageUrl,
+      file: imagePath,
     });
     console.log(req.body);
     console.log(req.file)
@@ -38,9 +38,40 @@ courseRouter.post("/create", upload.single("image"), async (req, res) => {
   }
 });
 
+courseRouter.put('/update/:_id', upload.single("image"), async (req, res) => {
+  try {
+    const file = req.file;
+    console.log(file,"------")
+    let imagePath
+    console.log("bfre",req.body)
+    if(file){
+     const  originalname = req.file.originalname 
+     const link = req.protocol + '://' + req.get('host')
+     imagePath = `${link}/${originalname}`    
+    }
+    else{
+      imagePath = AddCourse.file
+    }
+    console.log(req.params)
+    const user = await AddCourse.findByIdAndUpdate(req.params._id, {
+        title: req.body.title,
+        content: req.body.content,
+        video: req.body.video,
+        file: imagePath
+    }, {
+      new: true
+    })
+    res.send(user);
+    console.log("user",user)
+  } catch (e) {
+    res.status(400).send(e);
+    console.log(res.status(400).send(e));
+  }
+})
 
 courseRouter.get("/view", async (req, res) => {
   try {
+    
     const user = req.params
     const users = await AddCourse.find(user);
     res.send(users)
@@ -55,19 +86,6 @@ courseRouter.get("/view/:_id", async (req, res) => {
     res.send(user)
   } catch (e) {
     res.status(400).send(e);
-  }
-})
-courseRouter.put('/update/:_id', upload.single("image"), async (req, res) => {
-  try {
-    console.log(req.params)
-    const user = await AddCourse.findByIdAndUpdate(req.params._id, req.body, {
-      new: true
-    })
-    res.send(user);
-    console.log(req.body)
-  } catch (e) {
-    res.status(400).send(e);
-    console.log(res.status(400).send(e));
   }
 })
 
